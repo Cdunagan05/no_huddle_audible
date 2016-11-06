f2bcc4051fab24280df617317e6ee6ae53a3364d
Chase Dunagan <cdunagan@utexas.edu>
2016-11-05 21:04:37 -0600

    fixing redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wws://no-huddle-audible.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['http://no-huddle-audible.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://no-huddle-audible.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
